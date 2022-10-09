0e3a4be4cc8724a3bfee39ba50d2fc9ab1c3207a
zhongtaoaki <zhongtaodylan@163.com>
2022-10-09 14:33:25 +0900

    ArrayList

--- src/fifthWeek/ListDemo.java
+++ src/fifthWeek/ListDemo.java
@@ -6,8 +6,19 @@ import java.util.List;
 /**
  * List
  * ArrayList是实装了List这个接口的实体类，new的时候使用多态
- * List里面所有的元素只能是同一种数据类型，声明的时候可以指定
+ * List里面所有的元素只能是同一种数据类型，声明的时候可以指定，只能是引用数据类型
  * List的长度是可以改变的
+ * 
+ * 包装类 ラッパークラス
+ * int Integer
+ * byte Byte
+ * short Short
+ * long Long
+ * char Charater
+ * float Float
+ * double Double
+ * boolean Boolean
+ * 基本数据类型，是可以自动转换成对应的包装类的，自动装箱 auto boxing
  * */
 public class ListDemo {
     
@@ -31,6 +42,17 @@ public class ListDemo {
         System.out.println(list.get(0)); // IndexOutOfBoundsException
 
         System.out.println(list.size());
+
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 
 }
